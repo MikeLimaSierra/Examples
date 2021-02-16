9e150c55b5435e2db5fee8f2256b8ca16547cfcd
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-02-16 21:34:44 +0100

    interfaces: basic implementation

---
+++ Blog/2021-02-Interfaces/CommunicationModule/CommunicationModule/IDataChannel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace CommunicationModule {
+    public interface IDataChannel : INotifyPropertyChanged, IDisposable {
+
+        // properties
+
+        // methods
+
+        // …
+
+    }
+}
