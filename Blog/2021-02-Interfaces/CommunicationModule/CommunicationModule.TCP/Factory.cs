88eb063fcf251e525bdc2f8be1bbcd91eb5287e9
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-02-16 21:54:31 +0100

    interfaces: extended contracts with specialized implementations

---
+++ Blog/2021-02-Interfaces/CommunicationModule/CommunicationModule.TCP/Factory.cs
@@ -0,0 +1,9 @@
+﻿using CommunicationModule.Network;
+
+namespace CommunicationModule.TCP {
+    public static class Factory {
+
+        public static INetworkDataChannel CreateDataChannel() => new TCPDataChannel();
+  
+    }
+}
---
+++ Blog/2021-02-Interfaces/CommunicationModule/CommunicationModule.TCP/TCPDataChannel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+using CommunicationModule.Network;
+
+namespace CommunicationModule.TCP {
+
+    // uses tdp as underlying protocol
+    internal class TCPDataChannel : INetworkDataChannel {
+
+        #region properties
+
+        #endregion
+
+        #region ctors
+
+        internal TCPDataChannel() {
+            // ...
+        }
+
+        #endregion
+
+        #region methods
+
+        #endregion
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged([CallerMemberName] String propertyName = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        #endregion
+
+        #region IDisposable
+
+        private Boolean _disposedValue;
+
+        protected virtual void Dispose(Boolean disposing) {
+            if(!_disposedValue) {
+                if(disposing) {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                _disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~DataChannel()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose() {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+
+    }
+}
