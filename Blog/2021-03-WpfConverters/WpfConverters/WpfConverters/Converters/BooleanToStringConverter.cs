6ed19cd65f2ea93ff7b6c4f5d916b8067145daa8
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-02-23 16:17:40 +0100

    converters: initial code

---
+++ Blog/2021-03-WpfConverters/WpfConverters/WpfConverters/Converters/BooleanToStringConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WpfConverters.Converters {
+    public class BooleanToStringConverter : IValueConverter {
+
+        public Object Convert(Object value, Type targetType, Object parameter, CultureInfo culture) {
+            if(value != null && value is Boolean boolean) {
+                return (boolean) ? "Pressed." : "Not pressed.";
+            }
+            return "I'm broken :-(";
+        }
+
+        public Object ConvertBack(Object value, Type targetType, Object parameter, CultureInfo culture) => throw new NotImplementedException();
+
+    }
+}
